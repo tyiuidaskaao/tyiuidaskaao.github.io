71afd028264f2a1f081c618c15c55dbd8520385c
KatsuyoshiNishi <nishi@gravis-consulting.com>
2015-08-24 09:44:37 +0900

    d
    d

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey:'AIzaSyCtTxD8SQYgWyAATKfNDvMxnMO06UrcPEw'
+  gcmAPIKey:''
 };
